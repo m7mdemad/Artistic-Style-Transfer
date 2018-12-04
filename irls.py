0bdec9e9ee2348d6b02f87ce43a5b1e7a14278d3
m7mdemad <omdaemad28@yahoo.com>
2018-12-04 02:38:08 +0200

    modify irls to match the required style synthesis

--- irls.py
+++ irls.py
@@ -6,30 +6,32 @@ def IRLS(R,X,Z):
 #     Z is matrix of style patches
     r = 0.8 # robust statistics value to use (from the research paper) 
     e = 1e-10 #some small value
-    limit = 20 # no. of iterations 
+    limit = 5 # no. of iterations 
     Xk = X
-    for i in range(1, limit):
-        for j in range(1, R.shape[0]): 
-#           caculate the weight   
-            w = np.power(np.power(R[:,j]*Xk-Z[:,j] + e,2),(r-2)/2)
-            w = np.sum(np.asarray(w), axis=0) # just make calculations faster
-        
-        Xk = np.power(R.transpose()*w*R + e,-1)*R.transpose()*w*Z # update Xk         
-#         print(Xk)
+    w,h = R.shape
+    unsampled_pixs = np.logical_not(R.sum(axis=1)>0) 
+    unsampled_pixs = unsampled_pixs.astype(float) # prevent black bar artifacts from gap
+    
+    for i in range(0, limit):
+        A=unsampled_pixs # prevent black bar artifacts from gap
+        B=Xk*unsampled_pixs
+        for j in range(0, h): 
+            Rlogical = np.where(R[:,j]==0,0,1) 
+            Xlogical = np.where(Rlogical==1,Xk,0)
+            w = np.power(np.sum(np.power(Xlogical-Z[:,j],2) + e),(r-2)/2)
+            A=A+w*R[:,j] # diag(R)
+            B=B+w*Z[:,j]
+            
+        Xk=(1./(A+1e-10))*B;         
     return Xk
  
     
-    
 R = [[1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [4,5,6]
     ]
-X = [[11,21,37],
-    [43,5,16],
-    [75,82,9]
+Z = [[11,2,13],
+    [14,5,16]
     ]
-Z = [[1,2,3],
-    [4,5,6],
-    [7,8,9]
-    ]
+X = [1, 21]
 X = IRLS(np.asarray(R),np.asarray(X),np.asarray(Z)) # can be IRLS(R,X,Z) directly if matrices are defined as np.array not list
+print(X)
