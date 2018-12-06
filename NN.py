de22e07b4b9c003015418b6dbad835bd81dc90c6
ibrahim <ibrahim.m.akrab@gmail.com>
2018-12-06 02:47:47 +0200

    change dealing with patches from vectors to matrices to match IRLS

--- NN.py
+++ NN.py
@@ -1,33 +1,55 @@
 import numpy as np
 from commonfunctions import *
 
-
 def get_patches(img, patch_sizes, subsampling_gaps):
     patches = []
     for i in range(len(patch_sizes)):
-        patch = get_patches_aux(img, patch_sizes=patch_sizes, subsampling_gaps=subsampling_gaps, index=i)
+        patch = get_patches_aux(img, patch_size=patch_sizes[i], subsampling_gap=subsampling_gaps[i])
         patches.append(patch)
     return patches
     
     
-def get_patches_aux (img, patch_sizes, subsampling_gaps, index):
-    patch_size = patch_sizes[index]
-    gap = subsampling_gaps[index]
-    vertical_patch_count    = (img.shape[0] - patch_size) // gap + 1
-    horizontal_patch_count   = (img.shape[1] - patch_size) // gap + 1
-    total_patch_count = vertical_patch_count * horizontal_patch_count
-    patches = np.empty((total_patch_count * 4, patch_size * patch_size * 3)) # 4 rotations, 3 channels
-    patch_index = 0
-    for v_index in range(vertical_patch_count):
-        for h_index in range(horizontal_patch_count):
-            patch = img[v_index * gap : v_index * gap + patch_size,
-                        h_index * gap : h_index * gap + patch_size]
-            for i in range(4):                          # consider all rotations of the patch
-                patches[patch_index + i] = np.rot90(patch, k=i).flatten()    # rgb for each pixel one after another
-            patch_index += 4
-            #show_images([patch])
-    return patches
+#def get_patches_aux (img, patch_sizes, subsampling_gaps, index):
+#    patch_size = patch_sizes[index]
+#    gap = subsampling_gaps[index]
+#    patches = 
+#    vertical_patch_count    = (img.shape[0] - patch_size) // gap + 1
+#    horizontal_patch_count   = (img.shape[1] - patch_size) // gap + 1
+#    total_patch_count = vertical_patch_count * horizontal_patch_count
+#    patches = np.empty((total_patch_count * 4, patch_size * patch_size * 3)) # 4 rotations, 3 channels
+#    patch_index = 0
+#    for v_index in range(vertical_patch_count):
+#        for h_index in range(horizontal_patch_count):
+#            patch = img[v_index * gap : v_index * gap + patch_size,
+#                        h_index * gap : h_index * gap + patch_size]
+#            for i in range(4):                          # consider all rotations of the patch
+#                patches[patch_index + i] = np.rot90(patch, k=i).flatten()    # rgb for each pixel one after another
+#            patch_index += 4
+#            #show_images([patch])
+#    return patches
+    
+from skimage.util import view_as_windows
 
+def get_patches_aux(img, patch_size, subsampling_gap): 
+    return view_as_windows(img, window_shape=(patch_size, patch_size, 3), step=subsampling_gap)[:,:,0]
+
+def flatten_patches(patches, rotate = True):
+    patches_shape = patches.shape
+    patch_count = patches_shape[0] * patches_shape[1]
+    pixels_count = patches_shape[2] * patches_shape[3] * patches_shape[4]
+    patch_index = 0
+    if rotate:
+        rotations = 4
+    else:
+        rotations = 1
+        
+    flat_patches = np.empty((patch_count * rotations, pixels_count))    # 4 rotations for each patch
+    for rotation in range(rotations):
+        for row in patches:
+            for patch in row:
+                flat_patches[patch_index] = np.rot90(patch, k=rotation, axes=(0,1)).ravel()
+                patch_index += 1        
+    return flat_patches
 
 from sklearn.decomposition import PCA
 
@@ -69,7 +91,7 @@ def apply_nearest_neighbor(patches, nbrs=None):
         nbrs.fit(patches)
         return nbrs
 
-    return nbrs.kneighbors(patches, return_distance=False)
+    return nbrs.kneighbors(patches, return_distance=False).T[0]
 
 
 """
--- workflow.py
+++ workflow.py
@@ -47,7 +47,7 @@ overlapping_patches: a flag to whether we want overlapping patches from image or
 """
 
 
-def image_to_patches(image, overlapping_patches=True, is_pyramid=True):
+def image_to_patches(image, overlapping_patches=True, is_pyramid=True, index = 0):
     if is_pyramid:
         patches_pyramid = []
         for img in image:
@@ -59,10 +59,28 @@ def image_to_patches(image, overlapping_patches=True, is_pyramid=True):
         return patches_pyramid
     else:
         if overlapping_patches is True:
-            patches = get_patches(image, patch_sizes=patch_sizes, subsampling_gaps=subsampling_gaps)
+            patches = get_patches_aux(image, patch_sizes=patch_sizes[index], subsampling_gaps=subsampling_gaps[index])
         else:
-            patches = get_patches(image, patch_sizes=patch_sizes, subsampling_gaps=patch_sizes)
+            patches = get_patches(image, patch_sizes=patch_sizes[index], subsampling_gaps=patch_sizes[index])
         return patches
+
+def get_nn_style_patch_from_indices(style_patches, nn_indices, estimation_p_shape):
+    s_p_shape       = style_patches.shape
+    s_patches_count   = s_p_shape[0] * s_p_shape[1]
+    nearest_patches = np.empty(estimation_p_shape)
+    nearest_patch_index = 0
+    for index in nn_indices:
+        rotation= index // s_patches_count
+        r       = index % s_patches_count
+        v_index = r % s_p_shape[1]
+        h_index = r % s_p_shape[0]
+        nn_row = nearest_patch_index % estimation_p_shape[1]
+        nn_col = nearest_patch_index // estimation_p_shape[1]
+        nearest_patches[nn_row,nn_col] = np.rot90(style_patches[v_index,h_index], k=rotation, axes=(0,1))
+        nearest_patch_index += 1
+    return nearest_patches
+        
+        
 
 """
     * takes style pyramid and converts it into patches for each layer and each patch size
@@ -79,14 +97,21 @@ def prepare_style_patches(style_pyramid):
     nbrs    = pd.DataFrame(index=["L" + str(i) for i in range(L_max + 1)], columns=patch_sizes)
     for layer in style_patches.index:
         for patch_size in style_patches.columns:
-            style_patches[patch_size][layer] , scaler_obj = apply_standard_Scaler(style_patches[patch_size][layer])
+            flat_patches = flatten_patches(style_patches[patch_size][layer])
+            flat_patches , scaler_obj = apply_standard_Scaler(flat_patches)
             scaler[patch_size][layer] = scaler_obj
-            style_patches[patch_size][layer], pca_obj = apply_pca(style_patches[patch_size][layer])
+            flat_patches, pca_obj = apply_pca(flat_patches)
             pca[patch_size][layer] = pca_obj
-            nbrs[patch_size][layer] = apply_nearest_neighbor(style_patches[patch_size][layer])
+            nbrs[patch_size][layer] = apply_nearest_neighbor(flat_patches)
 
     return scaler, pca, nbrs, style_patches
 
+def content_fusion(content, estimation, segmentation):
+    identity_matrix = np.identity(segmentation.shape[0])
+    return np.stack([((np.linalg.inv(segmentation[:,:,i] + identity_matrix))
+                      .dot(estimation[:,:,i] + segmentation[:,:,i].dot(c[:,:,i]))) for i in range(3) ], axis=(2))
+    
+    
 
 def main():
     
@@ -94,43 +119,37 @@ def main():
     style = io.imread(r"images/starry-night - small.jpg")
 
     # TODO: Add segmentation here
-    segmentation = None
+    segmentation = np.zeros_like(content)
     # show_images([content, style, segmentation])
     
     
     images = initialize(content, style, segmentation)
     scaler_objects, pca_objects, nn_objects, style_patches = prepare_style_patches(images['style'])
     for layer in reversed(style_patches.index):
-        # convert estimation image of the current layer to patches we can operate on
-        estimation_patches = pd.DataFrame(data=image_to_patches(images['estimation'][layer], is_pyramid=False)).transpose()
-        estimation_patches.columns , estimation_patches.index = patch_sizes, [layer]
-        for patch_size in style_patches.columns:
+        for index, patch_size in enumerate(style_patches.columns):
             # add another loop for IRLS itarations
             scaler  = scaler_objects[patch_size][layer]
             pca     = pca_objects[patch_size][layer]
             nn      = nn_objects[patch_size][layer]
-            style_p = style_patches[patch_size][layer]
-            estimation_p = estimation_patches[patch_size][layer]
- #           nn_style = np.zeros_like(estimation_image)
-            scaled_estimation_patches = apply_standard_Scaler(estimation_p, scaler=scaler)
-            reduced_estimation_patches = apply_pca(scaled_estimation_patches, pca=pca)
-            nn_indices = apply_nearest_neighbor(reduced_estimation_patches, nbrs=nn)
-            nn_reduced_style_patches = style_p[(nn_indices.T)[0]]
-            nn_unreduced_estimation_patches = apply_pca(nn_reduced_style_patches, pca=pca, inverse=True)
-            nn_style_patches = apply_standard_Scaler(nn_unreduced_estimation_patches, scaler=scaler, inverse=True)  # called Z in the paper
-            
-
-#            vertical_patch_count = nn_style.shape[0] // patch_size
-#            horizontal_patch_count = nn_style.shape[1] // patch_size
-#            patch_index = 0
-#            for v_index in range(vertical_patch_count):
-#                for h_index in range(horizontal_patch_count):
-#                    nn_style[v_index * patch_size: v_index * patch_size + patch_size,
-#                    h_index * patch_size: h_index * patch_size + patch_size] = \
-#                        nn_style_patches[patch_index].reshape((patch_sizes, patch_sizes, 3))
-#                    patch_index += 1
-
-            # nn_style image should be ready by now
+            current_style_patches       = style_patches[patch_size][layer]
+            for _ in range(I_alg):
+                # convert estimation image of the current layer to patches we can operate on
+                current_estimation_patches  = image_to_patches(images['estimation'][layer], is_pyramid=False)
+                flat_curr_estimation_patches= flatten_patches(current_estimation_patches, rotate=False)
+                scaled_estimation_patches = apply_standard_Scaler(flat_curr_estimation_patches, scaler=scaler)
+                reduced_estimation_patches = apply_pca(scaled_estimation_patches, pca=pca)
+                nn_indices = apply_nearest_neighbor(reduced_estimation_patches, nbrs=nn)
+                nn_patches = get_nn_style_patch_from_indices(current_style_patches, nn_indices, current_estimation_patches.shape)
+    
+    
+                # nn_style image should be ready by now
+                # content fusion
+                estimation_image = content_fusion(content, estimation, segmentation)
+                # color transfer
+                estimation_image = color_transform(estimation_image, images['style'][layer])
+                # domain transform filter
+                
+            # scaling up
             images['estimation'][list(images.index).index(layer) - 1] =\
                 rescale(estimated_image, 2, multichannel=True,anti_aliasing=True, mode='constant', cval=0)
 
