0a7ec6bed5844a5a8d6c42ce1d5895029ec6c69a
demhahmed <demha.ahmed@yahoo.com>
2018-12-13 13:43:53 +0200

    fix a bug in domain filter

--- Domain Transfer 2.py
+++ Domain Transfer 2.py
@@ -1,51 +1,64 @@
 import numpy as np
-import commonfunctions as cm
+from commonfunctions import * 
 import cv2
 
+def image_transpose(I):
 
-def  RecursiveFilter(I, D, sigma):
+    h, w, num_channels = I.shape
+
+    mT = np.zeros((w, h, num_channels), dtype=float)
+
+    for c in range(0, num_channels):
+        mT[:, :, c] = I[:, :, c].conj().T
+    return mT
+
+def RecursiveFilter(I, D, sigma):
 
     a = np.exp((-1 * np.sqrt(2) / sigma))
     F = I
-    V = np.power(a,D)
-    [h,w,num_channels] = np.shape(I)
-    for i in range (2,w-1):
-        for c in range(1,num_channels):
-            F[:, i, c] += np.multiply( V[:, i],(F[:, i - 1, c] - F[:, i, c]))
-    for i in range (w-1,-1,1):
+    V = np.power(a, D)
+    [h, w, num_channels] = np.shape(I)
+    for i in range(1, w):
+        for c in range(0, num_channels):
+            F[:, i, c] += np.multiply(V[:, i], (F[:, i - 1, c] - F[:, i, c]))
+    for i in range(w-2, -1, -1):
         for c in range(1, num_channels):
-            F[:, i, c] += np.multiply( V[:, i+1],(F[:, i + 1, c] - F[:, i, c]))
+            F[:, i, c] += np.multiply(V[:, i+1], (F[:, i + 1, c] - F[:, i, c]))
 
     return F
 
-def Iterative_C(img, sigma_s, sigma_r, N):
-    I = img.astype(float)
-    #[h,w,num_channels] = np.shape(I)
-    dIcdx = abs(np.diff(I, 1, 1))
-    dIcdy = abs(np.diff(I, 1, 0))
+def Iterative_C(img, sigma_s, sigma_r, N=3):
+    I = np.copy(img.astype(float))
+    h, w, c = I.shape
 
-    dIdx = np.sum(dIcdx,axis=2)
-    dIdy = np.sum(dIcdy, axis=2)
-    dHdx = 1 + np.multiply((sigma_s/ sigma_r ),dIdx)
-    dVdy = 1 + np.multiply((sigma_s / sigma_r) , dIdy)
+    dIcdx = np.diff(I, 1, 1)
+    dIcdy = np.diff(I, 1, 0)
+
+    dIdx = np.zeros((h, w))
+    dIdy = np.zeros((h, w))
+    for c in range(0, 3):
+        dIdx[:, 1:w] = dIdx[:, 1:w] + abs(dIcdx[:, :, c])
+        dIdy[1:h, :] = dIdy[1:h, :] + abs(dIcdy[:, :, c])
+
+    dHdx = 1 + (sigma_s/sigma_r) * dIdx
+    dVdy = 1 + (sigma_s/sigma_r) * dIdy
     dVdy = np.transpose(dVdy)
     F = I
-    s = np.multiply(sigma_s, np.sqrt(3))
-    for i in range (0,N):
-        # Compute the sigma value for this iteration 14
-        sigma_H_i =np.multiply((2 ** (N - i-1)) / ((np.sqrt((4 ** N) - 1))),s)
+
+    sigma_H = sigma_s
+    for i in range(0, N):
+        # Compute the sigma value for this iteration
+        sigma_H_i = sigma_H * np.sqrt(3) * (2 ** (N - (i + 1))) / np.sqrt((4**N) - 1)
         F = RecursiveFilter(F, dHdx, sigma_H_i)
-        F=np.transpose(F, (0,2,1))
+        F = image_transpose(F)
         F = RecursiveFilter(F, dVdy, sigma_H_i)
-        F = np.transpose(F,(0,2,1))
-    F = F.astype(dtype=int)
+        F = image_transpose(F)
+
     return F
 
-img = cv2.imread('statue.png')
+def test():
+    src = io.imread(r"images/house 2-small.jpg")/255
 
-sigma_s = 100
-sigma_r = 3
-
-f = Iterative_C(img, sigma_s, sigma_r,3)
-
-cm.show_images([img,f],["img","y rab"])
+    #src = cv2.resize(src, dsize=(400, 400), interpolation=cv2.INTER_CUBIC) #somtimes it gives weird error when the img resolution is too big XD
+    f = Iterative_C(src, 5, 0.2)
+    show_images([src, f], ["Original", "After Filter"])
